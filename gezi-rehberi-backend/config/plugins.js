60fb74255735005a245ba958b2f8f9e6a7514cb7
Tolga <gursoyytolga@@gmail.com>
2026-06-08 17:53:38 +0300

    fix: use Cloudinary environment variables

--- gezi-rehberi-backend/config/plugins.js
+++ gezi-rehberi-backend/config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('dga2sq0q4'),
+        api_key: env('686263637615331'),
+        api_secret: env('5OCBuDp0revvRa-lY8h-HhqjOiM'),
       },
       actionOptions: {
         upload: {},
