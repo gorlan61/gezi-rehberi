fb681a63504be1807720e90fdeaad406b5836a9f
Tolga <gursoyytolga@@gmail.com>
2026-06-08 18:07:13 +0300

    fix: configure Cloudinary provider for Strapi

--- gezi-rehberi-backend/config/plugins.ts
+++ gezi-rehberi-backend/config/plugins.ts
@@ -1,5 +1,17 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('dga2sq0q4'),
+        api_key: env('686263637615331'),
+        api_secret: env('5OCBuDp0revvRa-lY8h-HhqjOiM'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
